66858f02dac53bd4c89cbb354be904d788f4b80f
Reetro <wells.justin70@yahoo.com>
2026-04-20 23:22:38 -0600

    Created a basic window

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include <raylib.h>
 
 int main()
 {
-	std::cout << "Hello\n";
+	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
+	InitWindow(800, 600, "Terraria Clone");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+		ClearBackground(RAYWHITE);
+
+		DrawRectangle(50, 50, 100, 100, {255, 0, 0, 127});
+		DrawRectangle(75, 75, 100, 100, {0, 255, 0, 127});
+
+		EndDrawing();
+	}
 
 	return 0;
 }
